30b94ba48047acd1625655bafd609ed14654ce10
tomsmile99 <babyking2532@gmail.com>
2021-12-29 11:48:11 +0700

    update2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'; // Add this line
 import '../styles/globals.css'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
