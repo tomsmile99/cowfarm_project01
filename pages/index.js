c0fa50d3ace2a9d23552f419fd2d12efab3fa814
tomsmile99 <babyking2532@gmail.com>
2021-12-29 14:34:12 +0700

    update7

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 import MainLayout from '../src/components/layouts/MainLayout';
-import Image from 'next/image'
+import BannerSlide from '../src/components/inHome/BannerSlide';
+import ImageGallery from 'react-image-gallery';
+
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
+
 
 export default function Home({}) {
   return (
@@ -8,47 +26,32 @@ export default function Home({}) {
       description="ผลงานของ........"
       KeyWords="รับซื้อวัว,ขายวัว,น้ำเชื้อวัวพันธ์"
     >
-      <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div className="carousel-indicators">
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
-            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" />
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <Image src="/images/bannerTest_img1.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
-              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-12-14nogift65.jpg" 
-              width={3840}
-              height={1191} 
-              className="d-block w-100" 
-              alt="..." /> */}
-            </div>
-            <div className="carousel-item">
-              <Image src="/images/bannerTest_img2.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
-              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-04-30Banner_car300464.jpg" 
-              width={3840}
-              height={1191} 
-              className="d-block w-100" 
-              alt="..." /> */}
-            </div>
-            <div className="carousel-item">
-              <Image src="/images/bannerTest_img3.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
-              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-04-30Banner_agriculture300464.jpg" 
-              width={3840}
-              height={1191} 
-              className="d-block w-100" 
-              alt="..." /> */}
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true" />
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true" />
-            <span className="visually-hidden">Next</span>
-          </button>
+    <BannerSlide></BannerSlide>
+    {/* Content */}
+    <div className ="container p-3">
+      <div className ="row">
+        <div className ="col-md-12">
+          <h1>รับซื้อ-ขายวัวพันธ์นแท้</h1>
         </div>
+      </div>
+      <div className ="row">
+        <div className ="col-md-3 text-center">
+          Box 1
+        </div>
+        <div className ="col-md-3 text-center">
+          Box 2
+        </div>
+        <div className ="col-md-3 text-center">
+          Box 3
+        </div>
+      </div>
+      <div className = "row">
+        <div className = "col-md-6">
+          <ImageGallery items={images} thumbnailPosition="left" showPlayButton={false} showNav={true} />
+        </div>
+      </div>
+    </div>{/*div container */}
+
     </MainLayout>
   )
 }
---
+++ src/components/inHome/BannerSlide.js
@@ -0,0 +1,50 @@
+
+import Image from 'next/image'
+
+const BannerSlide = () => {
+    return (
+        <div id="carouselExampleIndicators" className="carousel slide carousel-fade" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={1} aria-label="Slide 2" />
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to={2} aria-label="Slide 3" />
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <Image src="/images/bannerTest_img1.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
+              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-12-14nogift65.jpg" 
+              width={3840}
+              height={1191} 
+              className="d-block w-100" 
+              alt="..." /> */}
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/bannerTest_img2.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
+              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-04-30Banner_car300464.jpg" 
+              width={3840}
+              height={1191} 
+              className="d-block w-100" 
+              alt="..." /> */}
+            </div>
+            <div className="carousel-item">
+              <Image src="/images/bannerTest_img3.jpg" width={3840} height={1391} className="d-block w-100" alt="..."/>
+              {/* <Image src="https://saksiam.com/images/bannerSlide/2021-04-30Banner_agriculture300464.jpg" 
+              width={3840}
+              height={1191} 
+              className="d-block w-100" 
+              alt="..." /> */}
+            </div>
+          </div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true" />
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true" />
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+    )
+}
+
+export default BannerSlide
